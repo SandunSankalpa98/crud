36d07c495ecff72df3a4c6bd62d58b5589e01c5b
A.G.Sandun Sankalpa <sandunsankalpa10@gmail.com>
2024-10-07 00:20:18 +0530

    Add paginator

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -49,6 +49,7 @@
                                 @endforeach
                             </tbody>
                         </table>
+                        {{ $categories->links() }}
                     </div>
                 </div>
             </div>
